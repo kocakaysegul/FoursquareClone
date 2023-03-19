6a0e97182b252b9eb5778e8ec9b91d5ab65b8707
kocakaysegul <aysegulkocak112@gmail.com>
2023-03-19 15:52:54 +0300

    remember user entry

--- FoursquareClone/PlacesVC.swift
+++ FoursquareClone/PlacesVC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 class PlacesVC: UIViewController  {
     
@@ -18,10 +19,27 @@ class PlacesVC: UIViewController  {
         // Do any additional setup after loading the view.
         
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
+        
+        navigationController?.navigationBar.topItem?.leftBarButtonItem = UIBarButtonItem(title: "LogOut", style: UIBarButtonItem.Style.plain, target: self, action: #selector(logoutButtonClicked))
     }
     
     @objc func addButtonClicked (){
         //segue
+    }
+                                                                                         
+                                                                                         
+    @objc func logoutButtonClicked(){
+        PFUser.logOutInBackground { error in
+            if error != nil {
+                let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
+                alert.addAction(okButton)
+                self.present(alert, animated: true)
+                
+            } else {
+                self.performSegue(withIdentifier: "toSignUpVC", sender: nil)
+            }
+        }
     }
     
 
