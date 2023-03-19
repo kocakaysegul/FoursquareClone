6f6150338a5d9b630a5b37dfe14c8dbca73a8e6f
kocakaysegul <aysegulkocak112@gmail.com>
2023-03-19 16:05:22 +0300

    AddPlaceVC created

---
+++ FoursquareClone/AddPlaceVC.swift
@@ -0,0 +1,33 @@
+//
+//  AddPlaceVC.swift
+//  FoursquareClone
+//
+//  Created by Ayşegül Koçak on 19.03.2023.
+//
+
+import UIKit
+
+class AddPlaceVC: UIViewController {
+    
+    @IBOutlet weak var placeNameText: UITextField!
+    
+    @IBOutlet weak var placeTypeText: UITextField!
+    
+    @IBOutlet weak var placeAtmosphereText: UITextField!
+    
+    @IBOutlet weak var placeImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+    @IBAction func nextButtonClicked(_ sender: Any) {
+    }
+    
+
+
+}
