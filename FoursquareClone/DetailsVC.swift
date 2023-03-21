faba576a2c61c2a29592464115ceea06d6c8f145
kocakaysegul <aysegulkocak112@gmail.com>
2023-03-21 10:46:59 +0300

    DetailsVC completed

---
+++ FoursquareClone/DetailsVC.swift
@@ -0,0 +1,37 @@
+//
+//  DetailsVC.swift
+//  FoursquareClone
+//
+//  Created by Ayşegül Koçak on 21.03.2023.
+//
+
+import UIKit
+import MapKit
+
+class DetailsVC: UIViewController {
+    
+    @IBOutlet weak var detailsImageView: UIImageView!
+    @IBOutlet weak var detailsNameLabel: UILabel!
+    @IBOutlet weak var detailsTypeLabel: UILabel!
+    @IBOutlet weak var detailsAtmosphereLabel: UILabel!
+    @IBOutlet weak var detailsMapView: MKMapView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
