a311cb07040ed6d9ddcf972a9c161ca4eeaaa66a
kocakaysegul <aysegulkocak112@gmail.com>
2023-03-21 11:42:02 +0300

    UITapGestureRecognizer added

--- FoursquareClone/AddPlaceVC.swift
+++ FoursquareClone/AddPlaceVC.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class AddPlaceVC: UIViewController {
+class AddPlaceVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var placeNameText: UITextField!
     
@@ -21,12 +21,29 @@ class AddPlaceVC: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        placeImageView.isUserInteractionEnabled = true
+        let recognizer = UITapGestureRecognizer(target: self, action: #selector(chooseImage))
+        placeImageView.addGestureRecognizer(recognizer)
     }
     
+    
+    @objc func chooseImage() {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        self.present(picker, animated: true, completion: nil)
+    }
     
     
     @IBAction func nextButtonClicked(_ sender: Any) {
         performSegue(withIdentifier: "toMapVC", sender: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        placeImageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
     }
     
 
