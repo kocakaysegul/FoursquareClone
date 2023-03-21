0449abea9aca4b8277af55448246b9ea021b3097
kocakaysegul <aysegulkocak112@gmail.com>
2023-03-21 14:51:51 +0300

    data fetched

--- FoursquareClone/PlacesVC.swift
+++ FoursquareClone/PlacesVC.swift
@@ -8,10 +8,12 @@
 import UIKit
 import Parse
 
-class PlacesVC: UIViewController  {
+class PlacesVC: UIViewController, UITableViewDelegate, UITableViewDataSource  {
     
     
     @IBOutlet weak var tableView: UITableView!
+    var placeNameArray = [String]()
+    var placeIDArray = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +23,38 @@ class PlacesVC: UIViewController  {
         navigationController?.navigationBar.topItem?.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.add, target: self, action: #selector(addButtonClicked))
         
         navigationController?.navigationBar.topItem?.leftBarButtonItem = UIBarButtonItem(title: "LogOut", style: UIBarButtonItem.Style.plain, target: self, action: #selector(logoutButtonClicked))
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        getDataFromParse()
     }
+    
+    func getDataFromParse() {
+        let query = PFQuery(className: "Places")
+        query.findObjectsInBackground { objects, error in
+            if error != nil {
+                self.makeAlert(titleInput: "Error", messageInput: error?.localizedDescription ?? "Error!")
+            } else {
+                if objects != nil {
+                    
+                    self.placeIDArray.removeAll(keepingCapacity: false)
+                    self.placeNameArray.removeAll(keepingCapacity: false)
+                    
+                    for object in objects! {
+                        if let placeName = object.object(forKey: "name") as? String {
+                            if let placeID = object.objectId as? String {
+                                self.placeNameArray.append(placeName)
+                                self.placeIDArray.append(placeID)
+                            }
+                        }
+                    }
+                    self.tableView.reloadData()
+                }
+            }
+        }
+    }
+    
     
     @objc func addButtonClicked (){
         //segue
@@ -32,10 +65,7 @@ class PlacesVC: UIViewController  {
     @objc func logoutButtonClicked(){
         PFUser.logOutInBackground { error in
             if error != nil {
-                let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
-                let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
-                alert.addAction(okButton)
-                self.present(alert, animated: true)
+                self.makeAlert(titleInput: "Error", messageInput: error?.localizedDescription ?? "Error!")
                 
             } else {
                 self.performSegue(withIdentifier: "toSignUpVC", sender: nil)
@@ -44,14 +74,21 @@ class PlacesVC: UIViewController  {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return placeIDArray.count
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = placeNameArray[indexPath.row]
+        return cell
+    }
+    
+    func makeAlert(titleInput : String, messageInput: String){
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default)
+        alert.addAction(okButton)
+        self.present(alert, animated: true)
+    }
 
 }
